2b0b4b34833c3dcbdb177a280752337765cbcda8
carl richard scutt <carlscutt@gmail.com>
2026-08-12 15:46:57 +0200

    Delete integrations directory

--- integrations/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Phoenix integration layer.
-
-This package contains adapters to external systems that Phoenix consumes.
-Business logic must never depend directly on third-party software.
-"""
--- integrations/agent_reach/__init__.py
+++
@@ -1,6 +0,0 @@
-from .adapter import doctor_json, find_executable
-
-__all__ = [
-    "doctor_json",
-    "find_executable",
-]
